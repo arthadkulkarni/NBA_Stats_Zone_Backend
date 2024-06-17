f32c65159f832871961a1973c9e7da4f8d2ff100
arthadkulkarni <arthadkulk@gmail.com>
2024-06-16 23:01:48 -0400

    init changes

--- my_flask_app/nba_stats_zone/__init__.py
+++ my_flask_app/nba_stats_zone/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@Texas@2014@localhost/Player_Stats'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@127.0.0.1:5000/Player_Stats'
 db = SQLAlchemy(app)
 
 from nba_stats_zone import routes
