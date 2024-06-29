655aa1eb088c623e0c1d8813fc43be2d7124df6a
arthadkulkarni <arthadkulk@gmail.com>
2024-06-29 12:00:56 -0400

    add flask cors

--- my_flask_app/nba_stats_zone/__init__.py
+++ my_flask_app/nba_stats_zone/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@127.0.0.1/Player_Stats'
 db = SQLAlchemy(app)
+CORS(app)
 
 from nba_stats_zone import routes
